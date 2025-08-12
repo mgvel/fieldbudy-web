01a07bfb631061e4de5d34462e92373657c9abac
ankush rai <ankush98rai@gmail.com>
2025-08-12 18:28:40 +0530

    changes in config file

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-// export const PROXY_BASE_URL = 'https://fieldbuddy-dev.premacg.com'
-export const PROXY_BASE_URL = 'http://localhost:3000'
+export const PROXY_BASE_URL = 'https://fieldbuddy-dev.premacg.com'
+// export const PROXY_BASE_URL = 'http://localhost:3000'
 
 export const API_BASE_URL = `${PROXY_BASE_URL}/api/v1`; 
 // export const API_BASE_URL = 'https://fieldbuddy-dev.premacg.com/api/v1'
