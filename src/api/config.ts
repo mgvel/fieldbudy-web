e0302d22361ba58e259ec6ecefa68e4122c6660d
ankush rai <ankush98rai@gmail.com>
2025-08-15 21:24:42 +0530

    site inspection completed

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-export const PROXY_BASE_URL = 'https://fieldbuddy-dev.premacg.com'
+export const PROXY_BASE_URL = 'http://18.116.49.104'
 // export const PROXY_BASE_URL = 'http://localhost:3000'
 
 export const API_BASE_URL = `${PROXY_BASE_URL}/api/v1`; 
