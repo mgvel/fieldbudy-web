4aaa7d3ca7e05eadd114c8f18a1ceb85465087be
ankush rai <ankush98rai@gmail.com>
2025-08-18 11:39:31 +0530

    replacing endpoint

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,4 @@
-export const PROXY_BASE_URL = 'https://api-fieldbuddy.premacg.com/'
+export const PROXY_BASE_URL = 'https://api-fieldbuddy.premacg.com'
 // export const PROXY_BASE_URL = 'http://localhost:3000'
 
 export const API_BASE_URL = `${PROXY_BASE_URL}/api/v1`; 
